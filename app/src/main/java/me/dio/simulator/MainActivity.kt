1336c6ad672ecf23b46720d4c7434b7913afe3c5
alxmart <alxmartinez@gmail.com>
2022-07-29 16:39:57 -0300

    Enabling viewBinding in MainActivity.kt and created MainActivityJava

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -1,12 +1,24 @@
 package me.dio.simulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //setContentView(R.layout.activity_main)
+        //val tvHello = findViewById<TextView>(R.id.tvHello)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TESTANDO =>  binding.tvHello.text = "Fazendo um teste"
+
     }
 }
 
