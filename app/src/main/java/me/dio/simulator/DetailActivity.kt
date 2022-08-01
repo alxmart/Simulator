c6e8a3ea232f6d0eca22ae4b3b57adc89ed8fbda
alxmart <alxmartinez@gmail.com>
2022-08-01 09:41:25 -0300

    Enabling viewBinding in DetailActivity.kt

--- app/src/main/java/me/dio/simulator/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -5,16 +5,13 @@ import android.os.Bundle
 import me.dio.simulator.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityDetailBinding.inflate(layoutInflater)
-
-
-
-
-        setContentView(R.layout.activity_detail)
+        setContentView(binding.root)
     }
 }
