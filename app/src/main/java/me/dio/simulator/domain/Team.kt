4dee20c23b382bf2c7aedaba737891ccbcdfa601
alxmart <alxmartinez@gmail.com>
2022-08-11 20:17:54 -0300

    Enabling Parcelize.

--- app/src/main/java/me/dio/simulator/domain/Team.kt
+++ app/src/main/java/me/dio/simulator/domain/Team.kt
@@ -1,9 +1,11 @@
 package me.dio.simulator.domain
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Team(
-
     @SerializedName("nome")
     val name: String,
     @SerializedName("forca")
@@ -11,7 +13,7 @@ data class Team(
     @SerializedName("imagem")
     val image: String,
     var score: Int?
-)
+) : Parcelable
 
 /*{
     var score: Int = 0
