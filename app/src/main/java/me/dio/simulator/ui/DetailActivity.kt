7b59b6fdb193de3d0fd0f6839248e3db2360225a
alxmart <alxmartinez@gmail.com>
2022-08-12 16:03:26 -0300

    Enabling match detail's team name and rating (stars)

--- app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
+++ app/src/main/java/me/dio/simulator/ui/DetailActivity.kt
@@ -37,10 +37,17 @@ class DetailActivity : AppCompatActivity() {
             Glide.with(this).load(it.homeTeam.image).into(binding.ivHomeTeam)
             binding.tvHomeTeamName.text = it.homeTeam.name
             binding.rbHomeTeamStars.rating = it.homeTeam.stars.toFloat()
+            if (it.homeTeam.score != null) {
+                binding.tvHomeTeamScore.text = it.homeTeam.score.toString()
+            }
+
 
             Glide.with(this).load(it.awayTeam.image).into(binding.ivAwayTeam)
             binding.tvAwayTeamName.text = it.awayTeam.name
             binding.rbAwayTeamStars.rating = it.awayTeam.stars.toFloat()
+            if (it.homeTeam.score != null) {
+                binding.tvAwayTeamScore.text = it.awayTeam.score.toString()
+            }
 
         }
     }
